75739a58c06a2aaea6f667319651b8c884e1bfbb
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-03-27 18:59:27 +0700

    Encryption Function

--- index.php
+++ index.php
@@ -2,3 +2,13 @@
 <?php 
 echo password_hash("Robby", PASSWORD_DEFAULT);
 ?>
+
+<!-- contoh penggunaan -->
+<?php
+$username = $_POST['username'];
+//enkripsi dulu
+$password = password_hash($_POST['username'], PASSWORD_DEFAULT);
+
+// query simpan
+mysqli_query("INSERT INTO user (username, password) VALUE ('$username','$password')");
+?>
